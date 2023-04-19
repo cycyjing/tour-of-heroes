a672a722bcea28fc699b9873b849903c88db38a4
cycyjing <cycycyjing@gmail.com>
2023-04-19 22:31:33 +1200

    tour of heroes project init

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return <>
+  <h1>Tour of Heroes</h1>
+  <button>Dashboard</button>
+  <button>Heroes</button>
+  </>
+}
+
+export default Header
