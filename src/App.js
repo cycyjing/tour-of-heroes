25cb91f56e184538f435b44fd71aab582bf1a4ab
cycyjing <cycycyjing@gmail.com>
2023-05-06 22:03:29 +1200

    create dashboard and heroes tabs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header';
+import Dashboard from './Dashboard/Dashboard';
+import Heroes from './Heroes/Heroes';
+import { useEffect } from 'react';
 
 function App() {
+  // console.log('outside--start')
+  useEffect(() => {
+    // console.log('inside')
+    // return () => console.log('inside--return')
+  },[])
+  // console.log('outside--end')
+
   return (
     <div className="App">
       <Header/>
+      {/* {console.log('render')} */}
+      <Dashboard/>
+      {/* <Heroes/> */}
     </div>
   );
 }
---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return <>
+    <div>Top Heroes</div>
+  </>
+}
+
+export default Dashboard
